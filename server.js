fded942851ad003677b7b8fb9e784b53d67c7237
Satwik Joel <86243758+nsatwik@users.noreply.github.com>
2022-12-01 18:04:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome December !');
+  res.send('Welcome world !');
 });
 
 app.listen(PORT, HOST);
